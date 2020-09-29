b0de35b819d087917df65a33a9b26c718f3870ab
ashok36 <69450623+ashok36@users.noreply.github.com>
2020-09-29 14:50:38 +0530

    Add files via upload

---
+++ Sort array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays; 
+  
+public class SortExample 
+{ 
+    public static void main(String[] args) 
+    { 
+     
+        int[] arr = {13, 7, 6, 45, 21, 9, 101, 102}; 
+  
+        Arrays.sort(arr); 
+  
+        System.out.printf("sorted arr[] : %s", 
+                          Arrays.toString(arr)); 
+    } 
+} 
